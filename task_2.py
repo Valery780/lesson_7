7a5475858e808db32da7353a58878b21bd53099a
Valery780 <70340887+Valery780@users.noreply.github.com>
2020-09-20 16:08:06 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,22 @@
+def temp_calc():
+    temp_value = int(input('Enter the value:\n'))
+    temp_type = input('Enter the type:\n')
+    if temp_type == 'C':
+        k = temp_value - 273.15
+        c = temp_value
+        f = int((temp_value + 459.67) / 1.8)
+        print(f'Kelvins {k},Celsius {c}, Fahrenheits {f}')
+    elif temp_type == 'K':
+        c = temp_value + 273.15
+        k = temp_value
+        f = int(temp_value - 32) / 1.8
+        print(f'Kelvins {k}, Celsius {c}, Fahrenheits {f}')
+    elif temp_type == 'F':
+        f = temp_value
+        k = int((temp_value + 459.67) / 1.8)
+        c = int((temp_value - 32) / 1.8)
+        print(f'Kelvins {k}, Celsius {c}, Fahrenheits {f}')
+
+temp_calc()
+
+
